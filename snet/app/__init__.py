8998bf4eb650da63aa404b3ef06b3bfe0a0bae58
logo1tip <evgeni.leschinski@gmail.com>
2021-10-14 17:11:01 +0300

    settings

--- snet/app/__init__.py
+++ snet/app/__init__.py
@@ -1,2 +1,83 @@
+import snet
+import argparse
+from datetime import date
+from typing import TypeVar
+from snet.conf import settings
+
+
+Args = TypeVar("Args")
+
+
+class SnetService:
+
+    @staticmethod
+    def create_parser() -> Args:
+        parser = argparse.ArgumentParser(
+            prog="Social network.",
+            description="Very cool social network.\n(c) Evgeny Leshchinsky {}".format(
+                date.today().year
+            ),
+            add_help=False,
+        )
+        parser.add_argument(
+            "-h",
+            "--help",
+            action="help",
+            help="Print this message.",
+        )
+        parser.add_argument(
+            "-v",
+            "--version",
+            action="version",
+            help="Current version.",
+            version=f"Snet: {snet.__version__}",
+        )
+        parser.add_argument(
+            "--host",
+            dest="host",
+            default="127.0.0.1",
+            help="Network address.",
+        )
+        parser.add_argument(
+            "--port",
+            dest="port",
+            default=8008,
+            help="Network port.",
+        )
+        parser.add_argument(
+            "-d",
+            "--debug",
+            dest="debug",
+            action="store_true",
+            help="Run application in DEBUG mode.",
+        )
+        parser.add_argument(
+            "-t",
+            "--tasks",
+            dest="tasks",
+            action="store_true",
+            help="Run bg tasks.",
+        )
+        parser.add_argument(
+            "-w",
+            "--wait",
+            dest="wait",
+            action="store_true",
+            help="Run bg tasks.",
+        )
+        return parser.parse_known_args()[0], parser.parse_known_args()[1]
+
+
+    def __init__(self) -> None:
+        self.arguments, self.vars = self.create_parser()
+
+    def load(self):
+        print(settings.DEBUG)
+        return self
+    
+    def run(self):
+        ...
+
+
 def run():
-    print("Work!")
+    SnetService().load().run()
---
+++ snet/conf/.config.py
@@ -0,0 +1,106 @@
+import pathlib
+import sys, os
+
+BASE_DIR = pathlib.Path(__file__).parent.parent.absolute()
+DEBUG = True
+
+LOG_DIR = BASE_DIR.parent.absolute() / ".log"
+
+# if sys.platform != "linux" and not LOG_DIR.exists():
+if not LOG_DIR.exists():
+    os.makedirs(LOG_DIR)
+
+MIDDLEWARES = []
+STARTUP = []
+SHUTDOWN = []
+TASKS = []
+DATABASE = {
+    "connections": {
+        "default": {
+            "engine": "tortoise.backends.asyncpg",
+            "credentials": {
+                "host": "127.0.0.1",
+                "port": 5432,
+                "user": "snet",
+                "password": "Parol'",
+                "database": "snet_data",
+                "minsize": 50,
+                "maxsize": 90 if DEBUG else 190,
+            }
+        }
+    },
+    "apps": {
+        # 
+    },
+    "timezone": "UTC",
+}
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": True,
+    "formatters": {
+        "standard": {"format": "[%(asctime)s] # %(levelname)s %(message)s"},
+        "extended": {
+            "format": (
+                "<[%(asctime)s LINE: %(lineno)-5d] # %(levelname)-8s"
+                "%(pathname)s %(funcName)s():> %(message)s"
+            )
+        },
+        "json": {
+            "format": (
+                '{"_level": "%(levelname)s", "_time": "%(asctime)s", "_thread": '
+                '%(thread)d, "_file": "%(pathname)s", "_func": "%(funcName)s()",'
+                ' "_line": %(lineno)d, "_message": "%(message)s", "_name": "%(name)s"}'
+            )
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "extended",
+            "stream": "ext://sys.stdout",
+        },
+        "syslog": {
+            "class": "logging.handlers.SysLogHandler",
+            "formatter": "extended",
+            "address": "/dev/log",
+            "facility": "local0",
+        }
+        if sys.platform == "linux"
+        else {
+            "class": "logging.handlers.RotatingFileHandler",
+            "formatter": "extended",
+            "filename": LOG_DIR / "all.log",
+            "maxBytes": 10485760,
+            "backupCount": 10,
+            "encoding": "utf8",
+        },
+        "except": {
+            "class": "logging.handlers.SysLogHandler",
+            "formatter": "extended",
+            "address": "/dev/log",
+            "facility": "local1",
+        }
+        if sys.platform == "linux"
+        else {
+            "class": "logging.handlers.RotatingFileHandler",
+            "formatter": "extended",
+            "filename": LOG_DIR / "exceptions.log",
+            "maxBytes": 10485760,
+            "backupCount": 10,
+            "encoding": "utf8",
+        },
+    },
+    "loggers": {
+        "snet.dev": {"level": "DEBUG", "handlers": ["console", "syslog"]},
+        "snet.syslog": {
+            "level": "DEBUG" if DEBUG else "WARNING",
+            "handlers": ["syslog"],
+        },
+        "snet.except": {"level": "WARNING", "handlers": ["except"]},
+    },
+}
+
+LOGGER = "fxt.dev" if DEBUG else "fxt.syslog"
+EXCEPTLOGGER = "fxt.except"
+ROOTURLS = "snet.web.root.urls"
